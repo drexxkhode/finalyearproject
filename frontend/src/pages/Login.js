d4b042636756a70ab1de11c2050e8a10e9399755
drexxkhode <Philipessien981@gmail.com>
2026-03-08 15:18:43 +0000

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
     setError("");
 
     try {
-      const res = await axios.post(" http://localhost:5000/api/auth/login", {
+      const res = await axios.post(" https://finalyearproject-fjo8.onrender.com/api/auth/login", {
         email,
         password,
       });
